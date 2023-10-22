1346660d85c2f5412a559a066f1c2d6a16c6667a
0xfabian <117050378+0xfabian@users.noreply.github.com>
2023-10-22 23:02:53 +0300

    Update rgb_led.ino

--- hw2/rgb_led.ino
+++ hw2/rgb_led.ino
@@ -1,34 +1,36 @@
+const int analog_max = 1023;
+const int digital_max = 255;
 
-const int r_led = 11;
-const int g_led = 10;
-const int b_led = 9;
+const int r_out = 11;
+const int g_out = 10;
+const int b_out = 9;
 
-const int r_pot = A0;
-const int g_pot = A1;
-const int b_pot = A2;
+const int r_in = A0;
+const int g_in = A1;
+const int b_in = A2;
 
 void setup() 
 {
-  pinMode(r_led, OUTPUT);
-  pinMode(g_led, OUTPUT);
-  pinMode(b_led, OUTPUT);
+  pinMode(r_out, OUTPUT);
+  pinMode(g_out, OUTPUT);
+  pinMode(b_out, OUTPUT);
 
-  pinMode(r_pot, INPUT);
-  pinMode(g_pot, INPUT);
-  pinMode(b_pot, INPUT);
+  pinMode(r_in, INPUT);
+  pinMode(g_in, INPUT);
+  pinMode(b_in, INPUT);
 }
 
-void link(int led_pin, int pot_pin)
+void link(int in_pin, int out_pin)
 {
-  int pot_value = analogRead(pot_pin);
-  int led_value = map(pot_value, 0, 1023, 0, 255);
+  int in_value = analogRead(in_pin);
+  int out_value = map(in_value, 0, analog_max, 0, digital_max);
 
-  analogWrite(led_pin, led_value);
+  analogWrite(out_pin, out_value);
 }
 
 void loop() 
 {
-  link(r_led, r_pot);
-  link(b_led, b_pot);
-  link(g_led, g_pot);
+  link(r_in, r_out);
+  link(b_in, b_out);
+  link(g_in, g_out);
 }
